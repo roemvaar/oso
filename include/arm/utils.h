c5fa20f82cf46a8a45e46faa151292049708c98d
Roberto Valenzuela <valenzuelarober@gmail.com>
2025-05-17 12:44:50 -0400

    syscalls: synchronous exception handler (from EL0 to EL1)

--- include/arm/utils.h
+++ include/arm/utils.h
@@ -12,4 +12,6 @@ extern void put32(unsigned long, unsigned int);
 extern void cpu_switch_to(struct task_struct *prev, struct task_struct *next);
 extern void ret_from_fork(void);
 
+extern void change_to_el0(void);
+
 #endif  /* ARM_UTILS_H_ */
